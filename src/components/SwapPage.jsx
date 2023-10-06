fbd6ae6fc04b92261c44b4aa8c51a53bb5b5254d
theogguu <38506698+theogguu@users.noreply.github.com>
2023-10-06 18:13:11 -0500

    Implemented DB fetching
    Implemented DB Fetching on SwapPage.jsx
    
    Changes (same as the PR):
    
    Exported a database reference (LMK if anyone has a better solution)
    utilities/fetch_data.js:
    
    Uses db reference to fetch data from the "users" collection and "AAAAAAAA" document on firestore
    Converts Object objects to Array objects
    Returns Object array or null if failure
    src/App.js
    
    useEffect() hook only updates once? not sure how it works I just GPTd that lol but if anyone knows please share
    calls fetchDataArray from fetch_data.js
    changes mapping logic slightly to use person.id instead of person.image (we can change the name any time)
    
    TODO:
    - Image fetching from storage
    - A method to actually input users and images into the database
    - A way to refer to each user with a unique ID (there can be 2 John Smiths)
    -- Similarly, a way to refer to each user's images with a unique ID

--- src/components/SwapPage.jsx
+++ src/components/SwapPage.jsx
@@ -1,45 +1,79 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SwapPage.css';
 import { Button, Card } from "react-bootstrap";
 import Sidebar from './SideBar';
 
+import { db } from '..';
+import { fetchDataArray } from '../utilities/fetch_data'
+
 
 const SwapPage = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const data = [
-    {
-      name: "John Doe",
-      image: "johndoe.jpeg",
-      "skills-have": ["guitar", "music composition"],
-      "skills-want": ["tennis"],
-    },
-    {
-      name: "Jane Smith",
-      image: "janedoe.jpeg",
-      "skills-have": ["programming", "graphic design"],
-      "skills-want": ["photography"],
-    },
-    {
-      name: "Robert Johnson",
-      image: "robertjohnson.jpeg",
-      "skills-have": ["cooking", "gardening"],
-      "skills-want": ["hiking"],
-    },
-    {
-      name: "Emily Davis",
-      image: "emilydavis.jpeg",
-      "skills-have": ["writing", "public speaking"],
-      "skills-want": ["yoga"],
-    },
-    {
-      name: "Michael Wilson",
-      image: "michaelwilson.jpeg",
-      "skills-have": ["photography", "video editing"],
-      "skills-want": ["sailing"],
-    },
-  ];
+  // const data = [
+  //   {
+  //     name: "John Doe",
+  //     image: "johndoe.jpeg",
+  //     "skills-have": ["guitar", "music composition"],
+  //     "skills-want": ["tennis"],
+  //   },
+  //   {
+  //     name: "Jane Smith",
+  //     image: "janedoe.jpeg",
+  //     "skills-have": ["programming", "graphic design"],
+  //     "skills-want": ["photography"],
+  //   },
+  //   {
+  //     name: "Robert Johnson",
+  //     image: "robertjohnson.jpeg",
+  //     "skills-have": ["cooking", "gardening"],
+  //     "skills-want": ["hiking"],
+  //   },
+  //   {
+  //     name: "Emily Davis",
+  //     image: "emilydavis.jpeg",
+  //     "skills-have": ["writing", "public speaking"],
+  //     "skills-want": ["yoga"],
+  //   },
+  //   {
+  //     name: "Michael Wilson",
+  //     image: "michaelwilson.jpeg",
+  //     "skills-have": ["photography", "video editing"],
+  //     "skills-want": ["sailing"],
+  //   },
+  // ];
+
+  const [data, setData] = useState([]);
+
+  // using useEffect like this calls fetchData() once rather than repeatedly!!!
+  // apparently useEffect doesn't allow async requests unless it's done this way
+  // any alternatives?
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // fetchDataArray() => [{data}] (from fetch_data.js)
+        const result = await fetchDataArray(db);
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // data follows this format:
+  // [
+  //   user0: {
+  //     id: str // (filepath of image)
+  //             //  needs to be changed to grab from firebase storage
+  //     name: str 
+  //     skills-have: str[]
+  //     skills-want: str[]
+  //   },
+  //   user1: {...},
+  //   user2: {...}
+  // ]
   
   return (
     <div>
