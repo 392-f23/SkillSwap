f2698457b534c637070e0510b448573eb02bed0f
dietkrause <dietmarkrause2025@u.northwestern.edu>
2023-10-06 09:54:37 -0500

    separate swappage component, add side nav bar, and profile description

--- src/components/SwapPage.jsx
+++ src/components/SwapPage.jsx
@@ -1,47 +1,59 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './SwapPage.css';
 import { Button, Card } from "react-bootstrap";
+import Sidebar from './SideBar';
 
 
 const SwapPage = () => {
-  const [count, setCount] = useState(0);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const data = [
     {
         "name": "John Doe",
         "image": "images.jpeg",
         "skills-have": ["guitar", "music composition"],
-        "skills-want": ["tennis"]
+        "skills-want": ["tennis"],
+        "description": "I'm a musician looking to learn tennis to play with my girlfriend."
     },
     {
         "name": "Jane Smith",
         "skills-have": ["programming", "graphic design"],
-        "skills-want": ["photography"]
+        "skills-want": ["photography"],
+        "description": "I'm taking a photography class and would like to practice with someone."
     },
     {
         "name": "Robert Johnson",
         "skills-have": ["cooking", "gardening"],
-        "skills-want": ["hiking"]
+        "skills-want": ["hiking"],
+        "description": "I'm looking for someone to go hiking with during the weekends."
     },
     {
         "name": "Emily Davis",
         "skills-have": ["writing", "public speaking"],
-        "skills-want": ["yoga"]
+        "skills-want": ["yoga"],
+        "description": "I need to chill out and would like to learn yoga."
     },
     {
         "name": "Michael Wilson",
         "skills-have": ["photography", "video editing"],
-        "skills-want": ["sailing"]
+        "skills-want": ["sailing"],
+        "description": "I'm looking for someone to go sailing with during the weekends."
     }
   ]
   
   return (
     <div>
+    <button className="sidebar-toggle-button" onClick={() => setSidebarOpen(!sidebarOpen)}>☰</button>
+    <Sidebar show={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <h1>SkillSwap</h1>
       {data.map((person, index) => (
         <div className='skill-cards'>
         <Card key={index} style={{ width: '18rem' }}>
           <Card.Img variant="top" src={person.image} />
+          <div className="image-overlay">
+            <div className="image-text">{person.description}</div>
+            </div>
           <Card.Body>
             <Card.Title>{person.name}</Card.Title>
             <Card.Text>
