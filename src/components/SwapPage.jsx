4112a22dd08948edf7a26576742734b67088b90c
dietkrause <dietmarkrause2025@u.northwestern.edu>
2023-10-06 14:28:52 -0500

    solved conflicts and added updated data object

--- src/components/SwapPage.jsx
+++ src/components/SwapPage.jsx
@@ -10,37 +10,36 @@ const SwapPage = () => {
 
   const data = [
     {
-        "name": "John Doe",
-        "image": "images.jpeg",
-        "skills-have": ["guitar", "music composition"],
-        "skills-want": ["tennis"],
-        "description": "I'm a musician looking to learn tennis to play with my girlfriend."
+      name: "John Doe",
+      image: "johndoe.jpeg",
+      "skills-have": ["guitar", "music composition"],
+      "skills-want": ["tennis"],
     },
     {
-        "name": "Jane Smith",
-        "skills-have": ["programming", "graphic design"],
-        "skills-want": ["photography"],
-        "description": "I'm taking a photography class and would like to practice with someone."
+      name: "Jane Smith",
+      image: "janedoe.jpeg",
+      "skills-have": ["programming", "graphic design"],
+      "skills-want": ["photography"],
     },
     {
-        "name": "Robert Johnson",
-        "skills-have": ["cooking", "gardening"],
-        "skills-want": ["hiking"],
-        "description": "I'm looking for someone to go hiking with during the weekends."
+      name: "Robert Johnson",
+      image: "robertjohnson.jpeg",
+      "skills-have": ["cooking", "gardening"],
+      "skills-want": ["hiking"],
     },
     {
-        "name": "Emily Davis",
-        "skills-have": ["writing", "public speaking"],
-        "skills-want": ["yoga"],
-        "description": "I need to chill out and would like to learn yoga."
+      name: "Emily Davis",
+      image: "emilydavis.jpeg",
+      "skills-have": ["writing", "public speaking"],
+      "skills-want": ["yoga"],
     },
     {
-        "name": "Michael Wilson",
-        "skills-have": ["photography", "video editing"],
-        "skills-want": ["sailing"],
-        "description": "I'm looking for someone to go sailing with during the weekends."
-    }
-  ]
+      name: "Michael Wilson",
+      image: "michaelwilson.jpeg",
+      "skills-have": ["photography", "video editing"],
+      "skills-want": ["sailing"],
+    },
+  ];
   
   return (
     <div>
