e50fcec296aec35e973d88fd78a9d4def99b93f8
George Mousa <gmousa@dhcp-10-105-178-255.wireless.northwestern.private>
2023-10-16 17:57:18 -0500

    remove unnecessary links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -18,8 +18,6 @@ const activation = ({isActive}) => isActive ? 'active' : 'inactive';
 
 const Navigation = () => (
   <nav className="d-flex">
-    <NavLink to="/" className={activation} end>Posts</NavLink>
-    <NavLink to="/users" className={activation} end>Users</NavLink>
     <AuthButton />
   </nav>
 );
