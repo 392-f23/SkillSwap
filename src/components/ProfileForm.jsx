24537676b627f4ad0d4cc944247dbe9e5a09f845
theogguu <stevengu.is.not@gmail.com>
2023-10-20 13:47:52 -0500

    added skills have levels option to profile form. changed styling and formatting

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -4,8 +4,8 @@ import './ProfileForm.css';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 const ProfileForm = ({ onProfileSubmit, user }) => {
-  const [skillsHave, setSkillsHave] = useState('');
   const [skillsWant, setSkillsWant] = useState('');
+  const [skillsHaveFields, setSkillsHaveFields] = useState([{ skill: '', level: 'beginner' }]);
   const [imageFile, setImageFile] = useState(null);
 
   const handleFileChange = (e) => {
@@ -13,6 +13,22 @@ const ProfileForm = ({ onProfileSubmit, user }) => {
     if (file) {
       setImageFile(file);
     }
+  };
+
+  const handleSkillsHaveChange = (index, field, value) => {
+    const newSkillsHaveFields = [...skillsHaveFields];
+    newSkillsHaveFields[index][field] = value;
+    setSkillsHaveFields(newSkillsHaveFields);
+  };
+
+  const addSkillsHaveField = () => {
+    setSkillsHaveFields([...skillsHaveFields, { skill: '', level: 'beginner' }]);
+  };
+
+  const removeSkillsHaveField = (index) => {
+    const newSkillsHaveFields = [...skillsHaveFields];
+    newSkillsHaveFields.splice(index, 1);
+    setSkillsHaveFields(newSkillsHaveFields);
   };
 
   const handleSubmit = async (e) => {
@@ -39,12 +55,13 @@ const ProfileForm = ({ onProfileSubmit, user }) => {
       name,
       image: imageUrl,
       email,
-      "skills-have": skillsHave.split(',').map(skill => skill.trim()),
-      "skills-want": skillsWant.split(',').map(skill => skill.trim())
+      "skills-want": skillsWant.split(',').map(skill => skill.trim()),
+      "skills-have": skillsHaveFields.map(field => field.skill.trim()),
+      "skills-have-levels": skillsHaveFields.map(field => field.level),
     });
 
-    setSkillsHave('');
     setSkillsWant('');
+    setSkillsHaveFields([{ skill: '', level: 'beginner' }]);
     setImageFile(null);
   };
 
@@ -52,17 +69,48 @@ const ProfileForm = ({ onProfileSubmit, user }) => {
     <div className='profileForm'>
       <Form onSubmit={handleSubmit}>
         <Form.Group>
-          <Form.Label>Skills Have (comma separated)</Form.Label>
-          <Form.Control required type="text" value={skillsHave} onChange={e => setSkillsHave(e.target.value)} />
+          <Form.Label>Skills Have</Form.Label>
+          {skillsHaveFields.map((field, index) => (
+            <div className="row">
+              <div key={index} className="input-skills-have">
+                <div className="col-7">
+                <Form.Control
+                  required
+                  type="text"
+                  value={field.skill}
+                  onChange={(e) => handleSkillsHaveChange(index, 'skill', e.target.value)}
+                />
+                </div>
+                <div className="col">
+                <Form.Control
+                  as="select"
+                  value={field.level}
+                  onChange={(e) => handleSkillsHaveChange(index, 'level', e.target.value)}
+                >
+                  <option value="beginner">Beginner</option>
+                  <option value="intermediate">Intermediate</option>
+                  <option value="advanced">Advanced</option>
+                </Form.Control>
+                </div>
+                <Button variant="danger" className="remove-skills-button" onClick={() => removeSkillsHaveField(index)}>-</Button>
+              </div>
+            </div>
+          ))}
+          <Button variant="success" className="add-skills-button" onClick={addSkillsHaveField}>+ Add Skill</Button>
         </Form.Group>
-        <Form.Group>
+
+
+        <Form.Group className="form-group">
           <Form.Label>Skills Want (comma separated)</Form.Label>
-          <Form.Control required type="text" value={skillsWant} onChange={e => setSkillsWant(e.target.value)} />
+          <Form.Control placeholder="coding,baking,etc..." required type="text" value={skillsWant} onChange={e => setSkillsWant(e.target.value)} />
         </Form.Group>
-        <Form.Group>
+        
+
+        <Form.Group className="form-group">
           <Form.Label>Profile Picture</Form.Label>
           <Form.Control type="file" onChange={handleFileChange} />
         </Form.Group>
+
         <Button type="submit">Create Profile</Button>
       </Form>
     </div>
